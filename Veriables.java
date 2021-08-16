61ae4c4b875790156241142ed36c78fe8ceefbe3
dulshandadallege <dulshandadallege@gmail.com>
2021-08-16 22:59:43 +0530

    first commit

---
+++ Veriables.java
@@ -0,0 +1,26 @@
+package java_basics;
+
+public class Veriables {
+
+	public static void main(String[] args) {
+		
+		int x;
+		x = 34;
+		
+		System.out.println(x);
+		
+		x = 45;
+		
+		System.out.println(x + 10);
+		
+		System.out.println(x);
+		
+		String words;
+		
+		words = "My name is Dulshan";
+		
+		System.out.println(words + " and I am live in Kandana!");
+
+	}
+
+}
